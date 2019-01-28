0c68079dc0e2285ba6126f4fed529a04ef27d8d4
Faaiz <faaizalikhan.linkites@gmail.com>
2019-01-28 17:46:20 +0530

    changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,52 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+
+// We'll need something for creating the slugs
+const slugify = require(`slugify`)
+const slugifyOptions = {
+  replacement: '-',
+  remove: /[$*_+~.()'"!\-:@]/g,
+  lower: true
+}
+
+// We'll use Gatsby's createPage API for generating the pages
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    // This is where we'll do most of our work
+    graphql(`
+    {
+        allContentfulGetsbyTest {
+          edges {
+            node {
+              id
+              firstname
+              age
+            }
+          }
+        }
+      }
+    `)
+    .then((result)=>{
+        if (result.errors) {
+            reject(result.errors)
+        }
+
+        const pageTemplate = path.resolve(`./src/templates/blog-post.js`)
+        result.data.allContentfulGetsbyTest.edges.map(edge => {
+            createPage({
+                path: `/blog/${slugify(edge.node.firstname, slugifyOptions)}/`,
+                component: slash(pageTemplate),
+                context: {
+                    id: edge.node.id
+                },
+            })
+        })
+
+        resolve()
+    })
+  })
+}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import * as PropTypes from "prop-types"
+
+const propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+class BlogPost extends React.Component{
+ render(){
+    const page = this.props.data.contentfulGetsbyTest
+    console.log('PAGWE DADAFAT', page)
+
+    return(
+        <div>
+            <Layout>
+            <SEO title="Surf Status Blog" />
+            <h1>Hi, this is the blog </h1>
+            <p>Welcome to page the blog, here is where the blog posts will be displayed</p>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>
+        </div>
+  
+    )
+ }
+}
+
+export default BlogPost
+
+export const pageQuery = graphql`
+    query pageQuery($id: String) {
+        contentfulGetsbyTest( id: { eq: $id } ) {
+            firstname
+            age
+        }
+    }
+`
